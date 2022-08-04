f3e1b43aa4fb547b91a07415f79b0bb010f13b6c
MashiPe <penafieldavid471@gmail.com>
2022-08-04 18:14:55 -0500

    Added admin configuration

--- src/main/java/com/example/calendario/CalendarioApplication.java
+++ src/main/java/com/example/calendario/CalendarioApplication.java
@@ -2,12 +2,14 @@ package com.example.calendario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class CalendarioApplication {
 
     public static void main(String[] args) {
